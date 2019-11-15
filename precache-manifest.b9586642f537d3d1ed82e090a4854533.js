684882bf5e82d19e5fe4e925f41dcd5d9531f945
shups <shuloh>
2019-11-15 15:08:24 +0800

    Updates

---
+++ precache-manifest.b9586642f537d3d1ed82e090a4854533.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cea1b8d972cbeca2690d82449131192",
+    "url": "/ethTw33t-app/index.html"
+  },
+  {
+    "revision": "e50381f47d1eb6bba9f3",
+    "url": "/ethTw33t-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f445b03575fdd1d36c90",
+    "url": "/ethTw33t-app/static/js/2.cd63dcd7.chunk.js"
+  },
+  {
+    "revision": "e50381f47d1eb6bba9f3",
+    "url": "/ethTw33t-app/static/js/main.7ad72625.chunk.js"
+  },
+  {
+    "revision": "9c8076aa839b888ce0ce",
+    "url": "/ethTw33t-app/static/js/runtime-main.c9a5d0bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ethTw33t/precache-manifest.12cdfa1beacfd263e7daeb9c56ae52b9.js"
+  "/ethTw33t-app/precache-manifest.b9586642f537d3d1ed82e090a4854533.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ethTw33t/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ethTw33t-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
